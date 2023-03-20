81a1c8766737b725b6b5d6a5359e17f5c688d9f3
mknyambura <kariuki.mercie@gmail.com>
2023-03-20 08:44:25 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
+  resources :dog_houses do
+    resources :reviews do
+      resources :comments
+    end
+    # get '/dog_houses/:dog_house_id/reviews', to: 'dog_houses#reviews_index'
+  end
   resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/dog_houses/:dog_house_id/reviews/:id', to: 'dog_houses#review'
 end
